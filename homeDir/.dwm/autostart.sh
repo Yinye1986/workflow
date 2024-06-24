0635f6aace6a0a624acb9c4ea5dc3125bb46fba6
Chris Hung <yinye1986@gmail.com>
2024-06-24 15:21:34 +0800

    bak

--- homeDir/.dwm/autostart.sh
+++ homeDir/.dwm/autostart.sh
@@ -7,15 +7,10 @@ feh --bg-fill --randomize /usr/share/backgrounds/archlinux/* &
 /bin/bash /home/chris/.dwm/dwm-status.sh &
 
 xclip -selection clipboard &
-picom &
 
-
-export XMODIFIERS=@im=fcitx
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
 fcitx5 &
 
-#setxkbmap us -variant colemak
-#xmodmap ~/.Xmodmap
+setxkbmap us colemak
+xmodmap ~/.Xmodmap
 
 exec dwm
