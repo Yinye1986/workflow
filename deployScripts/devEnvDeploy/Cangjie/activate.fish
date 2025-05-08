1dcac79d02694195fd48bb0cdef8fcf253a4f57e
Chris Hung <yinye1986@gmail.com>
2025-05-08 11:39:49 +0800

    bak

--- deployScripts/devEnvDeploy/Cangjie/activate.fish
+++ deployScripts/devEnvDeploy/Cangjie/activate.fish
@@ -1,1 +1,9 @@
-set -x PATH $PATH "/home/chris/APP/cangjie/bin"
+set -x CANGJIE_HOME "/home/chris/.app/cangjie"
+set -x LD_LIBRARY_PATH /home/chris/.app/cangjie/runtime/lib/linux_x86_64_llvm /home/chris/.app/cangjie/tools/lib $LD_LIBRARY_PATH
+# set -x LD_LIBRARY_PATH "/home/chris/.app/cangjie/runtime/lib/linux_x86_64_llvm"
+set -x PATH $PATH "/home/chris/.app/cangjie/bin"
+set -x PATH $PATH "/home/chris/.app/cangjie/tools/bin"
+# set -x PATH $PATH "/home/chris/.cjpm/bin"
+
+
+
